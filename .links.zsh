298a0283766e0570fb7ca040c7ed70ba4f491d36
ninrod <filipe.silva@gmail.com>
2015-10-08 01:18:39 -0300

    script de instalação dos links.

---
+++ .links.zsh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/zsh
+
+ln -s .dircolors/dircolors.256dark(:a) ~/.lscolors
+ln -s .zshrc(:a) ~/.zshrc
+ln -s .oh-my-zsh(:a) ~/.oh-my-zsh
+ln -s .vim(:a) ~/.vim
+ln -s .vimrc(:a) ~/.vimrc
+ln -s .tmux.conf(:a) ~/.tmux.conf
--- install_symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-ln -s .dircolors/dircolors.256dark ~/.lscolors
-ln -s .oh-my-zsh ~/.oh-my-zsh
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-ln -s .zshrc ~/.zshrc
