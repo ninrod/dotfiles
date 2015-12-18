a0fd6ef3a0a1b12843deeb13d13a63adb55b8bf0
ninrod <filipe.silva@gmail.com>
2015-12-17 22:59:00 -0200

    adding oh-my-zsh-extras symlinks

--- .links.zsh
+++ .links.zsh
@@ -7,3 +7,7 @@ ln -s .vim(:a) ~/.vim
 ln -s .vimrc(:a) ~/.vimrc
 ln -s .tmux.conf(:a) ~/.tmux.conf
 ln -s .gitconfig(:a) ~/.gitconfig
+
+ln -s oh-my-zsh-extras/nin-vi-mode(:a) ~/.oh-my-zsh/custom/plugins/nin-vi-mode
+ln -s oh-my-zsh-extras/zsh-syntax-highlighting(:a) ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s oh-my-zsh-extras/powerlevel9k(:a) ~/.oh-my-zsh/custom/themes/powerlevel9k
