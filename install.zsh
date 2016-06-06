2a8c08ee2e96b768dc571cb7293a02f7f198859d
ninrod <filipe.silva@gmail.com>
2016-06-05 22:19:15 -0300

    ensures dotpath is set; no more emacs (I don't have time for vim and emacs)

--- install.zsh
+++ install.zsh
@@ -3,7 +3,20 @@
 # Author: Filipe Silva (ninrod)
 # License: Same as VIM.
 
-# helper function to manage symlinks.
+# function to ensure that $DOTPATH is set on $options_file
+setdotpath() {
+  local option_file="$(realpath ~/.options/shell-options.conf)"
+  if grep -q '^DOTPATH=' $option_file; then
+    # dotpath is set. no need to do anything
+    return 0
+  fi
+
+  # dotpath is not set. appending $DOTPATH to $options_file
+  local dotpath="$( cd "$(dirname "$0")" ; pwd -P  )"
+  echo "DOTPATH=$dotpath" >> $option_file
+}
+
+# helper function to manage linkage.
 updatelinks() {
   #$1 is the symlink
   #$2 is the target
@@ -18,20 +31,28 @@ updatelinks() {
   ln -s ${2:a} $1
 }
 
+# vim, zsh and tmux
 updatelinks ~/.vim dot/vim
 updatelinks ~/.vimrc dot/vimrc
 updatelinks ~/.zshrc dot/zshrc
-updatelinks ~/.bashrc dot/bashrc
-updatelinks ~/.bash_profile dot/bash_profile
 updatelinks ~/.tmux.conf dot/tmux.conf
-updatelinks ~/.lscolors dot/dircolors/dircolors.256dark
-updatelinks ~/.agignore dot/agignore
 
 # neovim
 updatelinks ~/.config dot/config
 
-# emacs
-updatelinks ~/.emacs.d dot/emacs.d
+# colors for ls
+updatelinks ~/.lscolors dot/dircolors/dircolors.256dark
+
+# bash
+updatelinks ~/.bashrc dot/bashrc
+updatelinks ~/.bash_profile dot/bash_profile
 
 # git
 cp dot/gitconfig ~/.gitconfig
+
+# ag (the silver searcher)
+updatelinks ~/.agignore dot/agignore
+
+# ensure $DOTPATH is set.
+# $DOTPATH is the directory where the dotfiles repo was cloned.
+setdotpath
