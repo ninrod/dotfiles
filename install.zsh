df3a8b2dfca6e3b1e5d85d1b40527747e3f67ff7
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-06-07 19:55:55 -0300

    fix: adds missing terminal color vars to install.zsh

--- install.zsh
+++ install.zsh
@@ -3,6 +3,21 @@
 # Author: Filipe Silva (ninrod)
 # License: Same as VIM.
 
+# terminal colors {{{
+
+TC='\e['
+Rst="${TC}0m"
+Black="${TC}30m";
+Red="${TC}31m";
+Green="${TC}32m";
+Yellow="${TC}33m";
+Blue="${TC}34m";
+Purple="${TC}35m";
+Cyan="${TC}36m";
+White="${TC}37m";
+
+# }}}
+
 # function to remove file, if applicable. {{{
 removeifexists() {
   # the `-f` switch tests if the argument exists and is a regular file
