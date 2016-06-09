cfa27fce4288e33183c33143434ea0e865ab4f7e
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-06-09 18:19:50 -0300

    install vim plugins if not installed and more:
    - fix: git user info
    - vim: adds `s` bind to `za`

--- install.zsh
+++ install.zsh
@@ -17,13 +17,14 @@ Cyan="${TC}36m";
 White="${TC}37m";
 
 # }}}
-
 # retrieves the path to the script itself {{{
+
 SCRIPTPATH="$(cd "$(dirname "$0")"; pwd -P)"
 cd $SCRIPTPATH
-# }}}
 
+# }}}
 # function to ensure options_file exists {{{
+
 ensure_options_file() {
   if [[ ! -d ~/.options ]]; then
     mkdir ~/.options
@@ -35,9 +36,10 @@ ensure_options_file() {
     cp conf/options/shell-options.conf ~/.options
   fi
 }
-# }}}
 
+# }}}
 # function to ensure $DOTPATH is set on $options_file {{{
+
 ensure_dotpath() {
   ensure_options_file
 
@@ -50,9 +52,10 @@ ensure_dotpath() {
   # append $DOTPATH to $options_file
   echo "DOTPATH=$SCRIPTPATH" >> $options_file
 }
-# }}}
 
+# }}}
 # function to ensure no regular user file will be overwritten {{{
+
 verifylink() {
   local symlink=${1:a}
 
@@ -62,9 +65,10 @@ verifylink() {
     exit 1
   fi
 }
-# }}}
 
+# }}}
 # function to help with symlinkage {{{
+
 updatelinks() {
   local symlink=${1:a}
   local symlink_old_target=${1:A}
@@ -82,6 +86,31 @@ updatelinks() {
 
   ln -s $symlink_new_target $symlink
 }
+
+# }}}
+# function to ensure vim plugins are installed {{{
+
+ensure_vim_plugins() {
+  local plug_dir=$(realpath -m ~/.vim/plugged)
+  if [[ ! -d $plug_dir ]]; then
+    echo -e "installing ${Yellow}vim${Rst} plugins..."
+    vim +PlugInstall +qall
+  fi
+}
+
+# }}}
+# function to apply git information  {{{
+
+# if available through environment variables
+apply_git_info() {
+  if [[ -n ${GIT_BCB_NAME+x} ]]; then
+    git config --global user.name "$GIT_BCB_NAME"
+  fi
+  if [[ -n ${GIT_BCB_EMAIL+x} ]]; then
+    git config --global user.email "$GIT_BCB_EMAIL"
+  fi
+}
+
 # }}}
 
 setopt extended_glob
@@ -94,12 +123,11 @@ for file in dot/^*.cp; do
   updatelinks ~/.${file:t} $file
 done
 
-# TODO make this funciton backup user files to $SCRIPTPATH/tmp/bkp, if applicable
+# TODO make this function backup user files to $SCRIPTPATH/tmp/bkp, if applicable
 for file in dot/*.cp; do
   cp $file ~/.${file:t:r}
 done
 
 ensure_dotpath
-
-# update vim plugins
-vim +PlugInstall +qall
+ensure_vim_plugins
+apply_git_info
