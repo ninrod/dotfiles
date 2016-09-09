e730106e2a156c598e0f4c4caf33feb71b33d8ee
ninrod <filipe.silva@gmail.com>
2016-09-09 12:29:01 -0300

    update

--- conf/emacs.d/init.el
+++ conf/emacs.d/init.el
@@ -13,3 +13,20 @@
 	(package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/ninit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme zenburn-theme yasnippet which-key use-package try pyvenv ox-reveal org-bullets jedi htmlize highlight-indentation flycheck evil color-theme ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
