2632ef1e89cc61d3ca09b3a406c58411c7fde565
ninrod <filipe.silva@gmail.com>
2016-09-12 00:29:19 -0300

    cleanup init.el

--- dot/spacemacs.d/init.el
+++ dot/spacemacs.d/init.el
@@ -2,18 +2,7 @@
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
 
-(defun dotspacemacs/layers ()
-  (org-babel-load-file (expand-file-name "~/.spacemacs.d/layers.org"))
-)
-
-(defun dotspacemacs/init ()
-  (org-babel-load-file (expand-file-name "~/.spacemacs.d/settings.org")))
-
-(defun dotspacemacs/user-init ()
-  "Initialization function for user code.
-   It is called immediately after `dotspacemacs/init', before layer configuration executes.
-   This function is mostly useful for variables that need to be set before packages are loaded.
-   If you are unsure, you should try in setting them in `dotspacemacs/user-config' first.")
-
-(defun dotspacemacs/user-config ()
-  (org-babel-load-file (expand-file-name "~/.spacemacs.d/userconfig.org")))
+(defun dotspacemacs/layers () (org-babel-load-file (expand-file-name "~/.spacemacs.d/layers.org")))
+(defun dotspacemacs/init () (org-babel-load-file (expand-file-name "~/.spacemacs.d/settings.org")))
+(defun dotspacemacs/user-init ())
+(defun dotspacemacs/user-config () (org-babel-load-file (expand-file-name "~/.spacemacs.d/userconfig.org")))
