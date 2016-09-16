dc188e495f153098dd45ce1dddc723790801de23
ninrod <filipe.silva@gmail.com>
2016-09-15 22:51:47 -0300

    fix 'loading lisp code' problem

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -28,6 +28,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("26488f9304ea29009b19a3b0a2660932ef748d098cac25a9a4167561c598a86f" default)))
  '(package-selected-packages
    (quote
     (yasnippet flycheck htmlize ox-reveal smart-mode-line gruvbox-theme auto-complete counsel ivy avy org-bullets which-key try evil-leader evil org-plus-contrib use-package))))
