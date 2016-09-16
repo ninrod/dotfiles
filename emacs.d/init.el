da3d00d86bec02a43199d86eaf3d00f044137032
ninrod <filipe.silva@gmail.com>
2016-09-16 10:07:13 -0300

    fix spacemacs theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,20 +23,3 @@
   :defer 7)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/boot.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("26488f9304ea29009b19a3b0a2660932ef748d098cac25a9a4167561c598a86f" default)))
- '(package-selected-packages
-   (quote
-    (yasnippet flycheck htmlize ox-reveal smart-mode-line gruvbox-theme auto-complete counsel ivy avy org-bullets which-key try evil-leader evil org-plus-contrib use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
