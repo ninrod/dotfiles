1503ed2ad609cf3a09da2c9b46b272c316cfa936
ninrod <filipe.silva@gmail.com>
2016-10-05 09:19:56 -0300

    version emacs customize file

---
+++ emacs.d/emacs-customizations.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-search-module (quote evil-search))
+ '(package-selected-packages
+   (quote
+    (evil-args exec-path-from-shell ivy magit counsel-projectile projectile company hl-anything highlight-parentheses rainbow-delimiters all-the-icons neotree markdown-mode yasnippet which-key use-package try spacemacs-theme spaceline smart-mode-line restart-emacs ox-reveal org-plus-contrib org-bullets htmlize flycheck evil-magit evil-leader evil-commentary counsel bind-map avy auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
