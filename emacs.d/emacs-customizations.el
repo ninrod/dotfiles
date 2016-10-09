bb3d80cea59c97ab16b79ef6250ddad29a758e9e
ninrod <filipe.silva@gmail.com>
2016-10-09 18:52:46 -0300

    add multi-term

--- emacs.d/emacs-customizations.el
+++ emacs.d/emacs-customizations.el
@@ -6,7 +6,7 @@
  '(evil-search-module (quote evil-search))
  '(package-selected-packages
    (quote
-    (anzu eyebrowse flx vimrc-mode ranger js2-mode web-mode ag dockerfile-mode evil-args exec-path-from-shell ivy magit counsel-projectile projectile company hl-anything highlight-parentheses rainbow-delimiters all-the-icons neotree markdown-mode yasnippet which-key use-package try spacemacs-theme spaceline smart-mode-line restart-emacs ox-reveal org-plus-contrib org-bullets htmlize flycheck evil-magit evil-leader evil-commentary counsel bind-map avy auto-complete))))
+    (multi-term anzu eyebrowse flx vimrc-mode ranger js2-mode web-mode ag dockerfile-mode evil-args exec-path-from-shell ivy magit counsel-projectile projectile company hl-anything highlight-parentheses rainbow-delimiters all-the-icons neotree markdown-mode yasnippet which-key use-package try spacemacs-theme spaceline smart-mode-line restart-emacs ox-reveal org-plus-contrib org-bullets htmlize flycheck evil-magit evil-leader evil-commentary counsel bind-map avy auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
