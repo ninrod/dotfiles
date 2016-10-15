22fed8e2fb12733b50f67d5673b0d67276b56e30
ninrod <filipe.silva@gmail.com>
2016-10-15 16:15:25 -0300

    add sudo to script

--- scripts/vim/build.sh
+++ scripts/vim/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $VIM_SRC_DIR
-./configure --prefix=/usr --with-features=huge --enable-multibyte && make && make install
+./configure --prefix=/usr --with-features=huge --enable-multibyte && make && sudo make install
