a62267a8b62a6d83b08128dc6a499c9a3c8bae32
ninrod <filipe.silva@gmail.com>
2016-10-20 08:22:30 -0200

    dot is now symlink

--- install.zsh
+++ install.zsh
@@ -183,15 +183,15 @@ clonedep wellle/targets.vim             https://github.com/wellle/targets.vim.gi
 
 cd $GIT_ROOT
 
-for file in dot/*.symlink; do
+for file in symlink/*.symlink; do
   verifylink ~/.${file:t:r}
 done
-for file in dot/*.symlink; do
+for file in symlink/*.symlink; do
   updatelinks ~/.${file:t:r} $file
 done
 
 # TODO make this function backup user files to $SCRIPTPATH/tmp/bkp, if applicable
-for file in dot/*.copy; do
+for file in symlink/*.copy; do
   cp $file ~/.${file:t:r}
 done
 
