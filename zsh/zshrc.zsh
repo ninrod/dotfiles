d0775cfd66a2cb210832ee86dd45ede9a26ff206
ninrod <filipe.silva@gmail.com>
2016-10-20 23:07:26 -0200

    simplify zshrc.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,23 +1,16 @@
 # -*- mode: sh -*-
 
-# load user environment {{{
-
 SHELL_USER_CONFIG_FILE="${HOME}/.options/shell-options.conf"
 if [[ -e "$SHELL_USER_CONFIG_FILE" ]]; then
   . "$SHELL_USER_CONFIG_FILE"
 fi
 
 source $DOTPATH/zsh/envbootstrap.zsh
-
-# }}}
-
 source $DOTPATH/zsh/options.zsh
 source $DOTPATH/zsh/completions.zsh
-
 source $DOTPATH/zsh/termcolors.zsh
 source $DOTPATH/zsh/fixescapecodes.zsh
 source $DOTPATH/zsh/zleconfig.zsh
-
 source $DOTPATH/zsh/functions.zsh
 source $DOTPATH/zsh/alias.zsh
 source $DOTPATH/zsh/alias-git.zsh
@@ -25,13 +18,9 @@ source $DOTPATH/zsh/alias-dir.zsh
 source $DOTPATH/zsh/alias-docker.zsh
 source $DOTPATH/zsh/alias-emacs.zsh
 source $DOTPATH/zsh/alias-vim.zsh
-
 source $DOTPATH/zsh/prompt.zsh
-
 source $DOTPATH/zsh/ninplug.zsh
 source $DOTPATH/zsh/plugins.zsh
-
-
 
 # load step
 deps="${DOTPATH}/deps"
