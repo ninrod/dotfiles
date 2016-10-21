7491db76285f4efc19aeb8ac7f1fa7d8a1df81af
ninrod <filipe.silva@gmail.com>
2016-10-21 09:34:32 -0200

    rename install to boot dir

--- boot/boot.zsh
+++ boot/boot.zsh
@@ -6,7 +6,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P  )"
 cd $SCRIPTPATH
 GIT_ROOT=$(git rev-parse --show-toplevel)
 source $GIT_ROOT/zsh/termcolors.zsh
-source $GIT_ROOT/install/functions.zsh
+source $GIT_ROOT/boot/functions.zsh
 
 # source custom giturl definitions, if applicable
 hash_options=~/.options/hashdeps.zsh
---
+++ install.zsh
@@ -0,0 +1,1 @@
+boot/boot.zsh
