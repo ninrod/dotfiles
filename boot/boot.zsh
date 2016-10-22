1ffffc6056c26ef4cb076e52c862dcc20af68b60
ninrod <filipe.silva@gmail.com>
2016-10-22 01:15:46 -0200

    fix symlink order

--- boot/boot.zsh
+++ boot/boot.zsh
@@ -7,9 +7,11 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 source $GIT_ROOT/zsh/termcolors.zsh
 source $GIT_ROOT/boot/functions.zsh
 source $GIT_ROOT/boot/ningrab.zsh
+source $GIT_ROOT/boot/symlinks.zsh
 source $GIT_ROOT/boot/zsh-deps.zsh
 source $GIT_ROOT/boot/vim-deps.zsh
-source $GIT_ROOT/boot/symlinks.zsh
 
 ensure_dotpath
+source ~/.options/shell-options.conf
+
 apply_git_info
