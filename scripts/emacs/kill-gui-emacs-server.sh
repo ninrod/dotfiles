ffb07c274395e9a765a5e87819b9336e21987c78
ninrod <filipe.silva@gmail.com>
2016-10-23 21:56:32 -0200

    add emacs gui server kill script

---
+++ scripts/emacs/kill-gui-emacs-server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if ! ps -ef | ag 'macs.*\-\-daemon.*gui' > /dev/null; then
+  echo "emacs gui daemon is already stopped, nothing to do."
+  exit 0
+fi
+
+echo 'emacs gui server is up. proceeding to the kill'
+
+emacsclient.sh --server-file=gui -e "(kill-emacs)"; rc=$?
+
+if [[ $rc = 0 ]]; then
+  echo 'emacs gui server killed.'
+else
+  echo "there was a problem killing emacs gui server: $rc"
+fi
