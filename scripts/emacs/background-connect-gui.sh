d21c44d694e5716ac2abfbf25c3d078974323712
ninrod <filipe.silva@gmail.com>
2016-10-24 10:25:10 -0200

    add log folder

--- scripts/emacs/background-connect-gui.sh
+++ scripts/emacs/background-connect-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup connect-emacs-gui-daemon.sh "$@" > background-connect-gui.log 2>&1 & 
+nohup connect-emacs-gui-daemon.sh "$@" > ~/.dotfiles/scripts/emacs/log/background-connect-gui.log 2>&1 & 
