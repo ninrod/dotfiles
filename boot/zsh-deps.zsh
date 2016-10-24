fefce33b6d1a686cea79268e32de65772ace7acd
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-10-24 15:18:52 -0200

    remove rupa/z

--- boot/zsh-deps.zsh
+++ boot/zsh-deps.zsh
@@ -2,10 +2,9 @@
 source $GIT_ROOT/zsh/deps-location.zsh
 mkdir -p $deps && cd $deps
 
-ningrab Tarrasch/bd                   
-ningrab supercrabtree/k                   
-ningrab ninrod/docker-zsh-completion      
-ningrab zsh-users/zsh-completions         
-ningrab zsh-users/zsh-syntax-highlighting 
-ningrab rupa/z
+ningrab Tarrasch/bd
+ningrab supercrabtree/k
+ningrab ninrod/docker-zsh-completion
+ningrab zsh-users/zsh-completions
+ningrab zsh-users/zsh-syntax-highlighting
 ningrab seebi/dircolors-solarized
