1b7f474165c3945815e7ac04c383f0d8c8659ff7
ninrod <filipe.silva@gmail.com>
2016-10-23 22:37:15 -0200

    update alias once more

--- zsh/alias-emacs.zsh
+++ zsh/alias-emacs.zsh
@@ -2,11 +2,21 @@
 
 # main
 alias e="connect-emacs-terminal-daemon.sh"
+alias eg="connect-emacs-gui-daemon.sh"
 
-# emacs server management
+# emacs terminal daemon
 alias ed="terminal-emacs-daemon-up.sh"
 alias ek="kill-terminal-emacs-server.sh"
 alias er='ek && ed'
 
+# emacs gui daemon
+alias egd="gui-emacs-daemon-up.sh"
+alias egk="kill-gui-emacs-server.sh"
+alias egr='egk && egd'
+
+# inspect specific emacs daemon instances
 alias eu="ps -ef | ag 'macs.*\-\-daemon.*terminal'"
+alias egu="ps -ef | ag 'macs.*\-\-daemon.*gui'"
+
+# inspect all emacs instances 
 alias ep="ps -ef | ag macs"
