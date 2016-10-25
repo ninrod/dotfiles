4cef12466c971972aa2c1bb61df212088009352d
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-10-25 17:24:54 -0200

    tweak pj plugin

--- zsh/pj.zsh
+++ zsh/pj.zsh
@@ -1,43 +1,28 @@
-alias pjo="pj open"
-alias f="pj"
+alias f="project_jump"
 
 project_paths=~/.options/project_paths.zsh
 if [[ -e $project_paths ]]; then
   source $project_paths
 fi
 
-pj () {
-    emulate -L zsh
-
-    cmd="cd"
-    project=$1
-
-    if [[ "open" == "$project" ]]; then
-        shift
-        project=$*
-        cmd=${=EDITOR}
-    else
-        project=$*
+project_jump () {
+  emulate -L zsh
+  project=$1
+  for chosen_project ($PROJECT_PATHS); do
+    if [[ -d "$chosen_project" ]] && [[ "${chosen_project:t}" == "$project" ]] then
+      cd "$chosen_project"
+      return 0
     fi
-
-    for basedir ($PROJECT_PATHS); do
-        if [[ -d "$basedir/$project" ]]; then
-            $cmd "$basedir/$project"
-            return
-        fi
-    done
-
-    echo "No such project '${project}'."
+  done
+  echo "No such project '${project}'."
 }
 
-_pj () {
-    emulate -L zsh
-
-    typeset -a projects
-    for basedir ($PROJECT_PATHS); do
-        projects+=(${basedir}/*(/N))
-    done
-
-    compadd ${projects:t}
+_project_jump () {
+  emulate -L zsh
+  typeset -a projects
+  for project ($PROJECT_PATHS); do
+    projects+=(${project})
+  done
+  compadd ${projects:t}
 }
-compdef _pj pj
+compdef _project_jump project_jump
