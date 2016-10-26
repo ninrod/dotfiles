9178b83edaa5025aaa64d4bee310514c695ef471
ninrod <filipe.silva@gmail.com>
2016-10-26 09:34:05 -0200

    simplify plugin configuration

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,14 +4,6 @@ plugin_paths+=(ninrod/docker-zsh-completion)
 plugin_paths+=(zsh-users/zsh-completions)
 plugin_paths+=(supercrabtree/k)
 
-# zsh-users/zsh-syntax-highlighting complex configuration
+# zsh-users/zsh-syntax-highlighting configuration
 plugin_paths+=(zsh-users/zsh-syntax-highlighting)
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
-typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan,bold'
-ZSH_HIGHLIGHT_STYLES[command]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[function]='fg=yellow,bold'
-ZSH_HIGHLIGHT_STYLES[path]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]='fg=red'
