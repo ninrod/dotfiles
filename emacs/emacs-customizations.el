55a09309404e2ce3f7ae30832e682a5c4faf2b84
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-10-26 19:20:26 -0200

    tweaking web-mode: add tab expand functions. add emmet.

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -6,7 +6,7 @@
  '(evil-search-module (quote evil-search))
  '(package-selected-packages
    (quote
-    (git-gutter evil-escape orgbox origami darktooth-theme evil-anzu evil-indent-plus evil-visualstar evil-exchange evil-surround evil-matchit evil ivy-hydra hydra evil-numbers gitconfig-mode smartparens multi-term anzu eyebrowse flx vimrc-mode ranger js2-mode web-mode ag dockerfile-mode evil-args exec-path-from-shell ivy magit counsel-projectile projectile company hl-anything highlight-parentheses rainbow-delimiters all-the-icons neotree markdown-mode yasnippet which-key use-package try spacemacs-theme spaceline smart-mode-line restart-emacs ox-reveal org-plus-contrib org-bullets htmlize flycheck evil-magit evil-leader evil-commentary counsel bind-map avy auto-complete)))
+    (smart-tab evil-tabs less-css-mode emmet-mode git-gutter evil-escape orgbox origami darktooth-theme evil-anzu evil-indent-plus evil-visualstar evil-exchange evil-surround evil-matchit evil ivy-hydra hydra evil-numbers gitconfig-mode smartparens multi-term anzu eyebrowse flx vimrc-mode ranger js2-mode web-mode ag dockerfile-mode evil-args exec-path-from-shell ivy magit counsel-projectile projectile company hl-anything highlight-parentheses rainbow-delimiters all-the-icons neotree markdown-mode yasnippet which-key use-package try spacemacs-theme spaceline smart-mode-line restart-emacs ox-reveal org-plus-contrib org-bullets htmlize flycheck evil-magit evil-leader evil-commentary counsel bind-map avy auto-complete)))
  '(safe-local-variable-values (quote ((origami-fold-style . triple-braces)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
