921cfa8c993d1a1f81cd68fd6c0b75610b6e4cee
ninrod <filipe.silva@gmail.com>
2016-10-26 09:16:16 -0200

    use nin-vi-mode

--- zsh/zleconfig.zsh
+++ zsh/zleconfig.zsh
@@ -3,12 +3,12 @@ if [[ -z ${EMACS+x} ]]; then
 
   # simplest possible vi key configuration
   # no delays when switching keymaps
-  export KEYTIMEOUT=5
+  # export KEYTIMEOUT=5
   # bootstrap vi-mode
-  bindkey -v
+  # bindkey -v
 
   # much more complex vi keys configuration
-  # source ~/.zsh.d/nin-vi-mode.zsh
+  source ~/.zsh.d/nin-vi-mode.zsh
 else
   export EDITOR="emacsclient"
   bindkey -e
