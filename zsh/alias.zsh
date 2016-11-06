b8fac25f8fb08f83b1efbfed6c2380cee3b95d82
ninrod <filipe.silva@gmail.com>
2016-11-06 08:08:43 -0200

    remove unused alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -10,12 +10,9 @@ alias c='clear'
 alias q='exit'
 alias b='builtin cd ..'
 alias w='echo -e "$Blue ${"$(pwd)"/$HOME/~} ${Red}at ${Cyan}$(whoami)${Red}@${Yellow}$(hostname -s)$Red using $Yellow${0}$Purple ${DOT_PROMPT_CHAR:-$}${Rst}"'
-alias m='nman'
-alias t='tmux'
 
 # apps
 alias ag='ag --hidden --path-to-agignore=~/.agignore'
-alias ms='gfm-viewer'
 alias tarc='tar -zcvf file.tar.gz'
 alias tarx='tar -zxvf'
 
