de45de4d33a54073374f5c88025586147d82c0a1
ninrod <filipe.silva@gmail.com>
2016-11-14 15:33:03 -0200

    fix agignore option

--- scripts/emacs/build-emacs.sh
+++ scripts/emacs/build-emacs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
 print $EMACS_SRC_DIR
-builtin cd "$EMACS_SRC_DIR" && make distclean && make clean
-git checkout emacs-25.1 && ./autogen.sh && ./configure --with-ns && make install
+builtin cd "$EMACS_SRC_DIR" &&
+  make distclean &&
+  make clean &&
+  ./autogen.sh &&
+  ./configure --with-ns &&
+  make install
