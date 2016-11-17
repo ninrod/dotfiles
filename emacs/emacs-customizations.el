3f2b79bc34e935cd305109fb427b255066edaab0
ninrod <filipe.silva@gmail.com>
2016-11-17 10:05:04 -0200

    customize trailing whitespace face

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -13,4 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(trailing-whitespace ((t (:background "dark magenta" :foreground "#B3B3B3"))))
+ '(whitespace-trailing ((t (:background "red")))))
