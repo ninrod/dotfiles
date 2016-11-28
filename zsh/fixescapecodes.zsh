1abc7e4ccbc86a5e3dc0d7479b11b995e766616f
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-11-28 19:01:37 -0200

    update zsh escape code fixes

--- zsh/fixescapecodes.zsh
+++ zsh/fixescapecodes.zsh
@@ -1,11 +1,22 @@
 # various escape code fixes
 
-# home, end, delete and backspace
+# pgdown,pgup, home, end, delete, backspace and C-arrow keys
 bindkey "^[[1~" beginning-of-line # home key
+bindkey "^[[H" beginning-of-line # home key
 bindkey "^[[4~" end-of-line # end key
-bindkey "^[[3~" delete-char # delete key
+bindkey "^[[F" end-of-line # end key
+
 bindkey "^H" backward-delete-char # backspace key
 bindkey "^?" backward-delete-char # backspace key
+bindkey "^[[3~" delete-char # delete key
+
+bindkey "^[OD" backward-word # C-< in tmux
+bindkey "^[OC" forward-word # C-> in tmux
+bindkey "^[[1;5D" backward-word # C-< outside tmux
+bindkey "^[[1;5C" forward-word # C-> outside tmux
+
+bindkey "^[[5~" up-line-or-search # pgup
+bindkey "^[[6~" down-line-or-search #pgdown
 
 # numeric keypad return fix (enter)
 unamestr=$(uname)
