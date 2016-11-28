a1e4a3543a9efe7ce53252cebfc6ba1215334a11
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-11-28 19:30:09 -0200

    add my own fork of evil-surround

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -6,7 +6,8 @@ mkdir -p $emacs_deps && cd $emacs_deps
 ningrab jtbm37/all-the-icons-dired
 ningrab ninrod/spacemacs-theme
 ningrab ninrod/thin-melpa-mirror
-ningrab ninrod/zerodark-theme isearch-tweak # temporary branch, while I wait for #29 to merge
+ningrab ninrod/zerodark-theme
+ningrab ninrod/evil-surround
 ningrab noctuid/rectangle-ext.el
 ningrab supermomonga/evil-textobj-entire
 ningrab syl20bnr/spacemacs-elpa-mirror
