addb7e7a76d1b2dd7a8764d4f5d0a1ef360aee5a
ninrod <filipe.silva@gmail.com>
2016-11-28 12:23:36 -0200

    evil-replace-with-register is on melpa now

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -3,11 +3,10 @@ source ~/.zsh.d/constants.zsh
 
 mkdir -p $emacs_deps && cd $emacs_deps
 
-ningrab Dewdrops/evil-ReplaceWithRegister
 ningrab jtbm37/all-the-icons-dired
-ningrab ninrod/thin-melpa-mirror
-ningrab ninrod/zerodark-theme
 ningrab ninrod/spacemacs-theme
+ningrab ninrod/thin-melpa-mirror
+ningrab ninrod/zerodark-theme isearch-tweak # temporary branch, while I wait for #29 to merge
 ningrab noctuid/rectangle-ext.el
 ningrab supermomonga/evil-textobj-entire
 ningrab syl20bnr/spacemacs-elpa-mirror
