79165f10b01468cc776b11f15f83a009914d2ca0
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-11-29 15:41:52 -0200

    update emacs deps

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -4,10 +4,11 @@ source ~/.zsh.d/constants.zsh
 mkdir -p $emacs_deps && cd $emacs_deps
 
 ningrab jtbm37/all-the-icons-dired
-ningrab ninrod/spacemacs-theme
-ningrab ninrod/thin-melpa-mirror
-ningrab ninrod/zerodark-theme
-ningrab ninrod/evil-surround
 ningrab noctuid/rectangle-ext.el
 ningrab supermomonga/evil-textobj-entire
 ningrab syl20bnr/spacemacs-elpa-mirror
+
+ningrab ninrod/evil-surround
+ningrab ninrod/spacemacs-theme
+ningrab ninrod/thin-melpa-mirror
+ningrab ninrod/zerodark-theme
