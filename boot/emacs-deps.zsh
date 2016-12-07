0d451ed3afd1f66516544a43f5462196f4d6096f
ninrod <filipe.silva@gmail.com>
2016-12-07 09:07:21 -0200

    drop all-the-icons-dired

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -1,13 +1,16 @@
 source ~/.zsh.d/constants.zsh
 mkdir -p $emacs_deps && cd $emacs_deps
 
-ningrab jtbm37/all-the-icons-dired
+# mirrors
+ningrab syl20bnr/spacemacs-elpa-mirror
+ningrab ninrod/thin-melpa-mirror
+
+# core editing
 ningrab noctuid/rect-ext.el
 ningrab supermomonga/evil-textobj-entire
-ningrab syl20bnr/spacemacs-elpa-mirror
-
 ningrab ninrod/evil-surround
+
+# forked themes
 ningrab ninrod/spacemacs-theme
-ningrab ninrod/thin-melpa-mirror
 ningrab ninrod/zerodark-theme
 ningrab ninrod/gruvbox-theme
