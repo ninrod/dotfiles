c758c7d0fd7afee6c843235c03c18e9dbd1992e0
ninrod <filipe.silva@gmail.com>
2016-12-06 23:39:25 -0200

    try out my own gruvbox fork

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -1,6 +1,4 @@
-# fetch emacs deps
 source ~/.zsh.d/constants.zsh
-
 mkdir -p $emacs_deps && cd $emacs_deps
 
 ningrab jtbm37/all-the-icons-dired
@@ -12,3 +10,4 @@ ningrab ninrod/evil-surround
 ningrab ninrod/spacemacs-theme
 ningrab ninrod/thin-melpa-mirror
 ningrab ninrod/zerodark-theme
+ningrab ninrod/gruvbox-theme
