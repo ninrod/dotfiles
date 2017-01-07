ac4a7967c86bc6b66902445e8eb151b9d2555b4e
ninrod <filipe.silva@gmail.com>
2017-01-06 23:32:14 -0200

    add gitconfig command

--- boot/options/shell-options.zsh
+++ boot/options/shell-options.zsh
@@ -1,7 +1,10 @@
 # here you can configure your git information. uncomment the lines and place valid git information
 
+# uncomment these next 4 lines and place valid git information
 # GIT_USER_NAME="your git user name"
 # GIT_USER_EMAIL="your.git.user@email.com"
+# git config --global user.name  $GIT_USER_NAME
+# git config --global user.email $GIT_USER_EMAIL
 
 # declare zsh associative array
 # typeset -A urlhash
