1b08261fecabe2d53ec7532664167c1da5624b64
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2017-01-20 10:43:32 -0200

    add editorconfig

--- emacs/init.el
+++ emacs/init.el
@@ -1,51 +1,18 @@
 (require 'package)
+
 (setq package-enable-at-startup nil)
-
-;; using online archives on OSx
-
-(when (eq system-type 'darwin)
-
-  ; (setq package-archives
-  ;       '(("melpa" . "https://melpa.org/packages/")
-  ;         ("org"   . "http://orgmode.org/elpa/")
-  ;         ("gnu"   . "https://elpa.gnu.org/packages/")))
-
-  (setq package-archives '(("melpa" . "~/.emacs.d/mirror-elpa/melpa/")
-                           ("org"   . "~/.emacs.d/mirror-elpa/org/")
-                           ("gnu"   . "~/.emacs.d/mirror-elpa/gnu/")))
-
-
-)
-
-
-
-;; using ninrod's thin-elpa-mirror
-
-(when (eq system-type 'gnu/linux)
-
-  ;; using thin mirror
-  ;; (setq package-archives '(("melpa" . "~/.emacs.d/lisp/ninrod/thin-melpa-mirror")))
-
-  ;; using elpa-clone (fat mirror)
-  (setq package-archives '(("melpa" . "~/.emacs.d/mirror-elpa/melpa/")
-                           ("org"   . "~/.emacs.d/mirror-elpa/org/")
-                           ("gnu"   . "~/.emacs.d/mirror-elpa/gnu/")))
-
-  )
-
+;; (setq package-archives '(("melpa" . "~/.emacs.d/lisp/ninrod/thin-melpa-mirror")))
+(setq package-archives '(("melpa" . "~/.emacs.d/mirror-elpa/melpa/")
+                         ("org"   . "~/.emacs.d/mirror-elpa/org/")
+                         ("gnu"   . "~/.emacs.d/mirror-elpa/gnu/")))
 (package-initialize)
-
-;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; fetch latest version of `org-mode'
 (use-package org
   :pin org
   :ensure org-plus-contrib
-  :defer 7
-  )
+  :defer 7); fetch latest version of `org-mode'
 
-;; org-babel-load my config which is written in `org-mode' format
-(org-babel-load-file (expand-file-name "~/.emacs.d/boot.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/boot.org")); org-babel-load my `org-mode' formatted config
