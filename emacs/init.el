4d71608201e08e0ce4a84f1e90c983d3d7e453c8
ninrod <filipe.silva@gmail.com>
2017-02-01 22:26:40 -0200

    tweak init.el with gc optimizations in startup

--- emacs/init.el
+++ emacs/init.el
@@ -15,4 +15,19 @@
   :ensure org-plus-contrib
   :defer 7); fetch latest version of `org-mode'
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/boot.org")); org-babel-load my `org-mode' formatted config
+;; stolen from noctuid's mouseless workflow
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; don't garbage collect during initialization
+(setq gc-cons-threshold (* 100 1024 1024))
+
+(setq vc-follow-symlinks "t") ; prevent prompt when loading org file
+(org-babel-load-file "~/.emacs.d/boot.org")
+
+;; also stolen from noctuid's mouseless workflow
+;; reset to slightly higher than default; idle timer suggested by vermiculus
+(run-with-idle-timer
+ 10 nil
+ (lambda ()
+   (setq gc-cons-threshold (* 100 1024))
+   (message "gc-cons-threshold restored to %S"
+            gc-cons-threshold)))
