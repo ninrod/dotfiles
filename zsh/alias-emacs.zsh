c4300e66e2e3529651a5fbae1c28cd2109c44b38
ninrod <filipe.silva@gmail.com>
2017-02-09 10:58:54 -0200

    add smex

--- zsh/alias-emacs.zsh
+++ zsh/alias-emacs.zsh
@@ -1,12 +1,12 @@
 # -*- mode: sh -*-
 
 # standard
-alias emacs="emacs.sh"
-alias em="background-emacs.sh"
+# alias emacs="emacs.sh"
+alias emacs="background-emacs.sh"
 
 # daemon
-alias e="background-connect-emacs-daemon.sh"
-alias ed="up-emacs-daemon.sh"
-alias ek="kill-emacs-server.sh"
-alias er="ek && ed"
-alias eu="ps aux | ag 'macs.*\-daemon'"
+# alias e="background-connect-emacs-daemon.sh"
+# alias ed="up-emacs-daemon.sh"
+# alias ek="kill-emacs-server.sh"
+# alias er="ek && ed"
+# alias eu="ps aux | ag 'macs.*\-daemon'"
