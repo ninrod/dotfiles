e2b16f8d995bb58e77abb652b6e88c6e143e7b1f
ninrod <filipe.silva@gmail.com>
2017-02-11 23:33:08 -0200

    colorize emacs scripts output

--- scripts/emacs/up-emacs-daemon.sh
+++ scripts/emacs/up-emacs-daemon.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env zsh
+
+# author: Filipe Silva (ninrod)
+
+SCRIPTPATH=$(cd $(dirname $0); pwd -P) && cd $SCRIPTPATH
+GIT_ROOT=$(git rev-parse --show-toplevel)
+source $GIT_ROOT/zsh/termcolors.zsh
 
 if emacsclient.sh -e "(ignore)" > /dev/null 2>&1; then
-  echo "emacs daemon is already up, nothing to do."
+  echo -e "emacs daemon is already ${Green}up${Rst}, nothing to do."
   exit 0
 else
-  echo "emacs daemon is killed. Let us ressurect it..."
+  echo -e "emacs daemon is ${Red}killed${Rst}. Let us ${Green}ressurect${Rst} it..."
 fi
 
 emacs.sh --daemon; rc=$?
 if [[ $rc = 0 ]]; then
-  echo 'emacs daemon started. success.'
+  echo -e "emacs daemon ${Blue}started${Rst}. success."
 else
-  echo "there was a problem starting emacs daemon: $rc"
+  echo -e "there was a ${Red}problem${Rst} starting emacs daemon: $rc"
 fi
