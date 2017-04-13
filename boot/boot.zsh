e6020999d50b72ef556e51d2c2ad849c7c28e14b
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2017-04-13 20:28:35 -0300

    if my options is present, symlink it

--- boot/boot.zsh
+++ boot/boot.zsh
@@ -9,11 +9,22 @@ source $GIT_ROOT/boot/functions.zsh
 source $GIT_ROOT/zsh/constants.zsh
 source $GIT_ROOT/boot/symlinks.zsh
 
+
+OPTIONS=$(readlink -f ~/code/sources/options)
+LINK_OPTIONS=$(readlink -f ~/.options)
+if [[ ! -e  $LINK_OPTIONS ]] && [[ -e $OPTIONS ]]; then
+  echo "symlinking options"
+  echo "first arg: $OPTIONS"
+  echo "second arg: $LINK_OPTIONS"
+  ln -s $OPTIONS $LINK_OPTIONS
+fi
 ensure_dotpath
 source $SHELL_OPTIONS_FILE
+
 export DOTPATH
 
 source $GIT_ROOT/boot/ningrab.zsh $1
 source $GIT_ROOT/boot/zsh-deps.zsh
 source $GIT_ROOT/boot/vim-deps.zsh
 source $GIT_ROOT/boot/emacs-deps.zsh
+
