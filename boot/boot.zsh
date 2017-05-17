6bcdd46a86f05fe365d033f2830e6e48f5f063f9
ninrod <filipe.silva@gmail.com>
2017-05-16 23:03:17 -0300

    reorder boot sources

--- boot/boot.zsh
+++ boot/boot.zsh
@@ -4,10 +4,11 @@
 SCRIPTPATH=$(cd $(dirname $0); pwd -P) && cd $SCRIPTPATH
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-source $GIT_ROOT/boot/guard.zsh
 source $GIT_ROOT/zsh/termcolors.zsh
-source $GIT_ROOT/boot/functions.zsh $1
 source $GIT_ROOT/zsh/constants.zsh
+
+source $GIT_ROOT/boot/guard.zsh
+source $GIT_ROOT/boot/functions.zsh $1
 
 source $GIT_ROOT/boot/symlinks.zsh
 source $GIT_ROOT/boot/options.zsh
