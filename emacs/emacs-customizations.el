f8ac55ab4215f75e36badb309d7871145a201e76
ninrod <filipe.silva@gmail.com>
2017-05-31 00:09:58 -0300

    Revert "sync emacs customizations"
    This reverts commit 8337eb986e18421db76a3a7671a9fa34479bae8d.

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; stolen from https://redd.it/skj37
+ ;; '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+
  '(evil-search-module (quote evil-search))
  '(ibuffer-fontification-alist
    (quote
