b157517ebd3d581af27968cb5549efad11415dcd
ninrod <filipe.silva@gmail.com>
2017-06-16 19:13:23 -0300

    go

--- boot/options.zsh
+++ boot/options.zsh
@@ -1,6 +1,9 @@
 OPTIONS=$(cd $SCRIPTPATH; cd ../../; readlink -f options)
-LINK_OPTIONS=$(readlink -f ~/.options)
-if [[ ! -e $LINK_OPTIONS ]] && [[ -e $OPTIONS ]]; then
+LINK_OPTIONS=~/.options
+if [[ -e $OPTIONS ]]; then
+  echo "removing $LINK_OPTIONS"
+  [[ -d $LINK_OPTIONS ]] && rm -rf $LINK_OPTIONS
+  [[ -e $LINK_OPTIONS ]] && rm -f $LINK_OPTIONS
   echo "symlinking options"
   echo "first arg: $OPTIONS"
   echo "second arg: $LINK_OPTIONS"
