c6d52e0a1befdffe081c56ab89723accf7e4172f
ninrod <filipe.silva@gmail.com>
2017-06-16 22:52:49 -0300

    update dependencies

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -2,19 +2,14 @@ source ~/.zsh.d/constants.zsh
 mkdir -p $emacs_deps && cd $emacs_deps
 
 # mirrors
-ningrab ninrod/zipped-melpa-mirror
-ningrab ninrod/thin-melpa-mirror
+ningrab ninrod/rsynced-melpa
 
 # core editing
-ningrab ninrod/evil
+ningrab emacs-evil/evil
+ningrab emacs-evil/evil-surround
 ningrab noctuid/rect-ext.el
 ningrab ninrod/targets.el
-ningrab timcharper/evil-surround develop
 
 # forked themes
-ningrab ninrod/spacemacs-theme
-ningrab ninrod/zerodark-theme
 ningrab ninrod/gruvbox-theme
 
-# toys
-ningrab parkouss/speed-type
