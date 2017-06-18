201f85bfacf7a783f4c64311d432e77adad8a7cd
ninrod <filipe.silva@gmail.com>
2017-06-18 09:18:31 -0300

    use evil-ediff

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -56,7 +56,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(trailing-whitespace ((t (:background "dark magenta" :foreground "#B3B3B3"))))
- '(whitespace-trailing ((t (:background "red")))))
+ '(trailing-whitespace ((t (:background "dark magenta" :foreground "#B3B3B3")))))
 
 (put 'narrow-to-region 'disabled nil)
