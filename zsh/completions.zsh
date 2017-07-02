a7916b033f5139848a7064ccc7701a006b0b8513
ninrod <filipe.silva@gmail.com>
2017-07-02 18:38:53 -0300

    improve zsh completions

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -27,5 +27,7 @@ zstyle ':completion:*' matcher-list '' \
   'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
   'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}'
 
+zstyle ':completion:*:functions' ignored-patterns '_*'
+
 # project path completion
 source ~/.zsh.d/pj.zsh
