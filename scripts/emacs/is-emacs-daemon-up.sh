ad62279ac9d9ff029b9ad8f93503321e557527b8
ninrod <filipe.silva@gmail.com>
2017-07-03 21:41:08 -0300

    tweak script

--- scripts/emacs/is-emacs-daemon-up.sh
+++ scripts/emacs/is-emacs-daemon-up.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-emacsclient -e '(daemonp)' 2>&1 > /dev/null; rc=$?
-if [[ $rc == 0 ]]; then
-  echo 1
-else
-  echo 0
-fi
+# other: 
+
+emacsclient -e '(daemonp)' > /dev/null 2>&1 && echo  || echo 
