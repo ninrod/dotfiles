daa4ece7bcef5bd3fd95b1d18a4330debed9b0ed
ninrod <filipe.silva@gmail.com>
2017-07-10 15:48:29 -0300

    tweak line number functions

--- zsh/alias-emacs.zsh
+++ zsh/alias-emacs.zsh
@@ -1,16 +1,16 @@
 # -*- mode: sh -*-
 
 # emacs command
-alias em=emacs.sh
 
 # emacs client
-alias et="emacsclient.sh --tty"
-alias ec="emacsclient.sh"
+alias et="emacsclient --tty"
+alias ec=emacsclient
 
 # daemon
 alias e="background-connect-emacs-daemon.sh"
 alias ed="up-emacs-daemon.sh"
 alias ek="kill-emacs-server.sh"
 alias er="ek && ed"
-alias eu="emacsclient.sh -e '(daemonp)'"
-alias ei="emacsclient.sh -e '(emacs-init-time)'"
+alias eu="emacsclient -e '(daemonp)'"
+alias ei="emacsclient -e '(emacs-init-time)'"
+
