a1ba0d3dc0e6e1abc2e024fef968bd5ff7a934f8
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2017-07-20 16:58:36 -0300

    update git remote overwrites

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -5,13 +5,8 @@ mkdir -p $emacs_deps && cd $emacs_deps
 ningrab ninrod/rsynced-melpa
 
 # core editing
-ningrab emacs-evil/evil
-ningrab emacs-evil/evil-surround
 ningrab ninrod/targets.el
 ningrab noctuid/rect-ext.el
 
 # forked themes
 ningrab ninrod/gruvbox-theme
-ningrab ninrod/spacemacs-theme
-ningrab ninrod/zerodark-theme
-ningrab ninrod/emacs-leuven-theme
--- boot/vim-deps.zsh
+++ boot/vim-deps.zsh
@@ -6,7 +6,6 @@ ningrab bronson/vim-visual-star-search
 ningrab chaoren/vim-wordmotion
 ningrab ervandew/supertab
 ningrab jiangmiao/auto-pairs
-ningrab junegunn/vim-easy-align
 ningrab ninrod/vim-multiple-cursors
 ningrab tommcdo/vim-exchange
 ningrab tpope/vim-commentary
@@ -17,14 +16,10 @@ ningrab vim-scripts/ReplaceWithRegister
 ningrab wellle/targets.vim
 ningrab kana/vim-textobj-user
 ningrab kana/vim-textobj-entire
-ningrab whatyouhide/vim-textobj-xmlattr
 
 # fetch cosmetic deps
 ningrab ninrod/gruvbox develop # just while I wait for morhetz/gruvbox#150 to merge.
 ningrab itchyny/lightline.vim
-ningrab edkolev/tmuxline.vim
-ningrab luochen1990/rainbow
-ningrab vim-ruby/vim-ruby
 
 # pathogen installation
 PATHOGEN_FILE=$vim_deps/tpope/vim-pathogen/autoload/pathogen.vim
