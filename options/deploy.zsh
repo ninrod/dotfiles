c33a334cc1ed3ee15e390bf7015f909807a1b570
ninrod <filipe.silva@gmail.com>
2017-07-30 22:44:10 -0300

    clarify echo options/deploy.zsh echo output

--- options/deploy.zsh
+++ options/deploy.zsh
@@ -4,9 +4,7 @@ if [[ -e $OPTIONS ]]; then
   echo "removing $LINK_OPTIONS"
   [[ -d $LINK_OPTIONS ]] && rm -rf $LINK_OPTIONS
   [[ -e $LINK_OPTIONS ]] && rm -f $LINK_OPTIONS
-  echo "symlinking options"
-  echo "first arg: $OPTIONS"
-  echo "second arg: $LINK_OPTIONS"
+  echo "symlinking alternative options configuration"
   ln -s $OPTIONS $LINK_OPTIONS
 else
   echo "symlinking default option configuration"
