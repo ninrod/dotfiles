95c84052b9bd783571a56af0d3089721eb2d94e5
ninrod <filipe.silva@gmail.com>
2017-08-14 11:46:46 -0300

    add documentation

--- options/arch/scripts/julicloud.sh
+++ options/arch/scripts/julicloud.sh
@@ -4,4 +4,8 @@
 # first, go to mycloud and export the share as NFS
 # then, uncover the shares with: showmount -e server
 
+# if lsmod | grep nfs does not return 0, you may have updgraded the kernel. reboot.
+# you can test this with modprobe nfs
+
 sudo mount julicloud:/mnt/HD/HD_a2/filipe /mnt/julicloud
+
