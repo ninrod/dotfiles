c285a1817cb4e667a3dafe2af012fc84125dd6e0
ninrod <filipe.silva@gmail.com>
2017-08-23 22:37:05 -0300

    complement #113 fix. update readme and remove unused file.

--- zsh/plugins.zsh
+++
@@ -1,8 +0,0 @@
-# 3rd party plugin selection
-plugin_paths+=(ninrod/docker-zsh-completion)
-plugin_paths+=(ninrod/docker-compose-zsh-completion)
-plugin_paths+=(ninrod/zsh-bd)
-
-plugin_paths+=(zsh-users/zsh-completions)
-plugin_paths+=(zdharma/fast-syntax-highlighting)
-plugin_paths+=(ninrod/k)
