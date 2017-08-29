4b433ee5f57a66d3d6f1e6aadac8a0fc5767857a
ninrod <filipe.silva@gmail.com>
2017-08-28 22:37:52 -0300

    improve nvm configuration

--- options/arch/environment.zsh
+++ options/arch/environment.zsh
@@ -8,5 +8,21 @@ PATH="$LINUX_SCRIPTS:$PATH"
 export PATH
 
 #nvm
-# export NVM_DIR="$HOME/.nvm"
-# [[ -e $NVM_DIR/nvm.sh ]] && source $NVM_DIR/nvm.sh
+
+nvm_config() {
+  cd $(readlink -f ~/.dotfiles)
+  cd ..
+  [[ ! -e secrets/nvm.zsh ]] && return 0
+  source secrets/nvm.zsh
+  if [[ -z ${USE_NVM+x} ]] || [[ $USE_NVM = 0 ]]; then
+    return 0
+  fi
+  export NVM_DIR="$HOME/.nvm"
+  [[ -e $NVM_DIR/nvm.sh ]] && source $NVM_DIR/nvm.sh
+}
+
+
+curdir=$(pwd -P)
+nvm_config
+cd $curdir
+
