3a64a7e8724c9f328c0edd9952e724ac4c244a89
ninrod <filipe.silva@gmail.com>
2017-09-17 16:04:30 -0300

    add temp alias

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -4,3 +4,16 @@ alias pdf="evince"
 alias open=xdg-open
 alias mpdcli='ncmpcpp -b ~/.config/ncmpcpp/bindings.conf'
 alias locate='~/.options/arch/scripts/mylocate.zsh'
+
+function warsaw-browser {
+  xhost +
+  docker run -it --rm                                              \
+         -e DISPLAY=unix$DISPLAY                                   \
+         -v /tmp/.X11-unix:/tmp/.X11-unix                          \
+         -v $(readlink -f .):/opt/assets                           \
+         -v /dev/shm --tmpfs /dev/shm:rw,nosuid,nodev,exec,size=4g \
+         --name warsaw-browser                                     \
+         -w /opt/assets                                            \
+         lichti/warsaw-browser bb
+}
+alias bb="warsaw-browser bb"
