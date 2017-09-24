8bb9a2b12823e0e40d533f4583dd1445bf87d424
ninrod <filipe.silva@gmail.com>
2017-09-23 23:18:48 -0300

    add LD_LIBRARY_PATH hack

--- options/arch/environment.zsh
+++ options/arch/environment.zsh
@@ -9,6 +9,7 @@ PATH="$LINUX_SCRIPTS:$PATH"
 PATH="$CARGO_BIN:$PATH"
 export PATH
 
+# hack stolen from: https://github.com/rust-lang-nursery/rustfmt/issues/1687#issuecomment-308756211
 export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
 
 nvm_config() {
