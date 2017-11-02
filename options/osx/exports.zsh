fd5839eb34d674fb1b253dcc8b4d886dbd372a49
Filipe Silva <ninrod@Filipes-MacBook-Pro.local>
2017-11-01 23:47:09 -0200

    more fixes to path in osx

--- options/osx/exports.zsh
+++ options/osx/exports.zsh
@@ -11,22 +11,22 @@ CUSTOM_SCRIPTS=$EMACS_SCRIPTS:$ZSH_SCRIPTS:$VIM_SCRIPTS:$OSX_SCRIPTS:$GIT_SCRIPT
 GNUBIN_PATH=/usr/local/opt/coreutils/libexec/gnubin
 
 # Rust stuff
-CARGO_BIN=~/.cargo/bin
-RUST_SRC_PATH=~/code/sources/rust/src
+# CARGO_BIN=~/.cargo/bin
+# RUST_SRC_PATH=~/code/sources/rust/src
 
-# java stuff
-export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
-export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
-alias java7='export JAVA_HOME=$JAVA_7_HOME && mountpath'
-alias java8='export JAVA_HOME=$JAVA_8_HOME && mountpath'
-export JAVA_HOME=$JAVA_8_HOME
+# # java stuff
+# export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+# export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
+# alias java7='export JAVA_HOME=$JAVA_7_HOME && mountpath'
+# alias java8='export JAVA_HOME=$JAVA_8_HOME && mountpath'
+# export JAVA_HOME=$JAVA_8_HOME
 
 mountpath () {
   PATH="$GNUBIN_PATH:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin"
   PATH="$PATH:$CUSTOM_SCRIPTS"
-  PATH="$PATH:$JAVA_HOME/bin"
-  PATH="$PATH:$CARGO_BIN"
-  PATH="$PATH:$RUST_SRC_PATH"
+  # PATH="$PATH:$JAVA_HOME/bin"
+  # PATH="$PATH:$CARGO_BIN"
+  # PATH="$PATH:$RUST_SRC_PATH"
   export PATH
 }
 mountpath
