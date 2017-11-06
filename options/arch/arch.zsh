9246c541630d50f24f7389538861f308d33f8e02
ninrod <filipe.silva@gmail.com>
2017-11-06 01:06:56 -0200

    remove tmux

--- options/arch/arch.zsh
+++ options/arch/arch.zsh
@@ -2,4 +2,4 @@ source ~/.options/arch/environment.zsh
 source ~/.options/arch/favorites.zsh
 source ~/.options/arch/prompt.zsh
 source ~/.options/arch/alias.zsh
-source ~/.options/arch/scripts/tmux-attach.sh
+# source ~/.options/arch/scripts/tmux-attach.sh
