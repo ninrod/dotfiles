6a4d032b4f4da59b9daa43efeae45d1905d23180
ninrod <filipe.silva@gmail.com>
2017-11-28 00:23:29 -0200

    add evil-describe-inline

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -46,6 +46,34 @@
  '(safe-local-variable-values
    (quote
     ((eval when
+           (and
+            (buffer-file-name)
+            (file-regular-p
+             (buffer-file-name))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (unless
+               (featurep
+                (quote package-build))
+             (let
+                 ((load-path
+                   (cons "../package-build" load-path)))
+               (require
+                (quote package-build))))
+           (package-build-minor-mode)
+           (set
+            (make-local-variable
+             (quote package-build-working-dir))
+            (expand-file-name "../working/"))
+           (set
+            (make-local-variable
+             (quote package-build-archive-dir))
+            (expand-file-name "../packages/"))
+           (set
+            (make-local-variable
+             (quote package-build-recipes-dir))
+            default-directory))
+     (eval when
            (fboundp
             (quote rainbow-mode))
            (rainbow-mode 1))
