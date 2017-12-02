fea613a1ac3f2d0fb7df8bf04edde25d4cd2b4f4
ninrod <filipe.silva@gmail.com>
2017-12-01 23:42:27 -0200

    simplify emacs deps file

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -1,13 +1,8 @@
 mkdir -p $emacs_deps && cd $emacs_deps
 
-# mirrors
 ningrab ninrod/rsynced-melpa
-
-# core editing
 ningrab noctuid/rect-ext.el
+ningrab ninrod/gruvbox-theme
 
 # ningrab ninrod/evil
 # ningrab ninrod/targets.el
-
-# forked themes
-ningrab ninrod/gruvbox-theme
