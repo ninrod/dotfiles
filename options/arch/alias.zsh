38e7e73cbd5ec9b8bbb61e6761413ea3b4466d57
ninrod <filipe.silva@gmail.com>
2017-12-06 23:23:32 -0200

    update warsaw browser bootstrap

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -10,13 +10,13 @@ alias nm='sudo netctl stop-all; sudo netctl start motorola'
 
 function warsaw-browser {
   xhost +
-  docker run -it --rm                                              \
-         -e DISPLAY=unix$DISPLAY                                   \
-         -v /tmp/.X11-unix:/tmp/.X11-unix                          \
-         -v $(readlink -f .):/opt/assets                           \
-         -v /dev/shm --tmpfs /dev/shm:rw,nosuid,nodev,exec,size=4g \
-         --name warsaw-browser                                     \
-         -w /opt/assets                                            \
+  docker run -it --rm                     \
+         -e DISPLAY=unix$DISPLAY          \
+         -v /tmp/.X11-unix:/tmp/.X11-unix \
+         -v $(readlink -f .):/opt/assets  \
+         --shm-size 2g                    \
+         --name warsaw-browser            \
+         -w /opt/assets                   \
          lichti/warsaw-browser bb
 }
 alias bb="warsaw-browser bb"
