05940ae58d5b8b34b0bb0cd7d789ed1317156d9d
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-01-24 20:19:26 -0200

    add marcus scripts

---
+++ scripts/zsh/fssh.zsh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# to use, define a hosts file $servers with the following contents
+# blahblah.com - My delicious server
+# ilovemyserver.org - My other server
+# so.on.and.so.forth - ...
+
+user=myuser
+servers=/root/.secret/servers_quick_list
+description=$(cat $servers | fzf)
+server=$(echo $description | cut -d "-" -f1)
+
+[[ -n $server ]] && exec ssh $server -l $user
+
