a56caeacdab2e303c4f64825c7be8fb67dd564d7
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-02-08 15:00:00 -0200

    tweak zsh history configuration

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -5,18 +5,12 @@ WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 HISTFILE=$HOME/.zsh_history
 HISTSIZE=2000000
 SAVEHIST=2000000
-
 setopt append_history
 setopt extended_history
 setopt share_history
 setopt hist_verify
-
-# I actually want to profile my command usage
-# setopt hist_ignore_all_dups
-
-# I do not care if I entered a command with a leading space
-# I want it to be stored
-# setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
 
 # dir navigation options
 setopt auto_pushd
