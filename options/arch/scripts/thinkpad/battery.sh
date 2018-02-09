180e92a9a2832ac701b8c555d1aaee248c96e2cc
ninrod <filipe.silva@gmail.com>
2018-02-08 23:51:37 -0200

    add credits

--- options/arch/scripts/thinkpad/battery.sh
+++ options/arch/scripts/thinkpad/battery.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# stolen from polybar-scripts:
+# https://github.com/x70b1/polybar-scripts/blob/master/polybar-scripts/battery-combined-tlp/battery-combined-tlp.sh
+
 battery=$(sudo tlp-stat -b | tail -2 | head -n 1 | tr -d -c "[:digit:],.")
 
 echo "# $battery %"
