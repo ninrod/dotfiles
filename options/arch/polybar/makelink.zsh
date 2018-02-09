6bf11e1741ceae755b32fdacd9fe99523a7cf739
ninrod <filipe.silva@gmail.com>
2018-02-09 08:14:39 -0200

    add udev thinkpad battery status approach

--- options/arch/polybar/makelink.zsh
+++ options/arch/polybar/makelink.zsh
@@ -15,3 +15,16 @@ mkdir -p $DIR
 
 ln -sf $(readlink -f $FILE) $(readlink -f $SYMLINK)
 ln -sf $(readlink -f $LAUNCHER) $(readlink -f $FULL_LAUNCHER)
+
+thinkpad_battery_udev_rule() {
+  local src_dir=$(readlink -f scripts/thinkpad_battery_udev)
+  local dst_dir=/etc/udev/rules.d
+  local filename=95-battery.rules
+  local src_file=$src_dir/$filename
+  local dst_file=$dst_dir/$filename
+  if [[ -e $dst_file ]]; then
+    sudo rm $dst_file
+  fi
+  sudo ln -s $src_file $dst_file
+}
+thinkpad_battery_udev_rule
