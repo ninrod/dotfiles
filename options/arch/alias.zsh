c2f138186fa4dc6a18ed7c6d9fe228385a898fac
ninrod <filipe.silva@gmail.com>
2018-02-08 23:12:01 -0200

    remove warsaw alias. add bcbWifi alias. improve tlp alias

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -1,25 +1,12 @@
 alias xclip="xclip -selection c"
-alias bat="sudo tlp stat | tail -9"
+alias bat="sudo tlp stat -b"
 alias pdf="evince"
 alias open=xdg-open
 alias mpdcli='ncmpcpp -b ~/.config/ncmpcpp/bindings.conf'
 
 alias nj='sudo netctl stop-all; sudo netctl start juliforever'
 alias ns='sudo netctl stop-all; sudo netctl start s8'
-
-warsaw-browser() {
-  xhost +
-  docker run -it --rm                     \
-         -e DISPLAY=unix$DISPLAY          \
-         -v /tmp/.X11-unix:/tmp/.X11-unix \
-         -v $(readlink -f .):/opt/assets  \
-         --shm-size 2g                    \
-         --name warsaw-browser            \
-         -w /opt/assets                   \
-         lichti/warsaw-browser bb
-}
-alias bb="warsaw-browser bb"
-
+alias nb='sudo netctl stop-all; sudo netctl start bcbWifi'
 
 # conda
 alias sa='source activate'
