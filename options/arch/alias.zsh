1f5273d7b850e3c8cea5693192a4660622b1ad69
ninrod <filipe.silva@gmail.com>
2018-02-16 23:17:09 -0200

    add pacman alias

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -1,13 +1,26 @@
+# pacman
+
+alias pmfs='pacman -Fs'  # query file database. a whatprovides local
+alias pmss='pacman -Ss'      # search for packages
+alias pmqm='pacman -Qm'      # list foreign, manually installed
+alias pmquent='pacman -Qent' # list all explicit installed packages
+alias pmqmdt='pacman -Qmdt'  # foreign and orphans and installted as deps
+alias pms='sudo pacman -S'   # install
+alias pmu='sudo pacman -Syu' # update
+
+# conda
+alias sa='source activate'
+alias sd='source deactivate'
+
+# random
 alias xclip="xclip -selection c"
 alias bat="sudo tlp stat -b"
 alias pdf="evince"
 alias open=xdg-open
 alias mpdcli='ncmpcpp -b ~/.config/ncmpcpp/bindings.conf'
 
+# netctl
 alias nj='sudo netctl stop-all; sudo netctl start juliforever'
 alias ns='sudo netctl stop-all; sudo netctl start s8'
 alias nb='sudo netctl stop-all; sudo netctl start bcbWifi'
 
-# conda
-alias sa='source activate'
-alias sd='source deactivate'
