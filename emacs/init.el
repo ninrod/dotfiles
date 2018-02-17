b1dfcb0f48497ac0932cd66332417b8daae37995
ninrod <filipe.silva@gmail.com>
2018-02-17 02:47:03 -0200

    update circe config

--- emacs/init.el
+++ emacs/init.el
@@ -15,17 +15,31 @@
   (use-package org
     :pin org
     :ensure org-plus-contrib
-    :defer 7); fetch latest version of `org-mode'
+    :defer 7)                     ; fetch latest version of `org-mode'
 
   (setq vc-follow-symlinks "t") ; prevent prompt when loading org file
-  (org-babel-load-file (expand-file-name "boot.org" user-emacs-directory))
 
-  (let ((secrets-file "secrets/emacs.org"))
+(defun ninrod/parent-dir (dirname times)
+        "get the parent dir of DIRNAME"
+        (let* ((path (file-truename dirname)))
+          (dotimes (number times path)
+            (setq path (file-name-directory (directory-file-name path))))
+          path))
+  (let ((secrets-file "secrets/emacs-pre.org"))
     (cond ((file-exists-p (concat (ninrod/parent-dir user-emacs-directory 2) secrets-file))
-           (message "secrets EXISTS!!! loading...")
+           (message "pre-secrets EXISTS!!! loading...")
            (org-babel-load-file (expand-file-name secrets-file "~/code/sources")))
           (t
-           (message "secrets does NOT exist. nothing to see here."))))
+           (message "pre-secrets does NOT exist. nothing to see here."))))
+
+  (org-babel-load-file (expand-file-name "boot.org" user-emacs-directory))
+
+  (let ((secrets-file "secrets/emacs-post.org"))
+    (cond ((file-exists-p (concat (ninrod/parent-dir user-emacs-directory 2) secrets-file))
+           (message "post-secrets EXISTS!!! loading...")
+           (org-babel-load-file (expand-file-name secrets-file "~/code/sources")))
+          (t
+           (message "post-secrets does NOT exist. nothing to see here."))))
 
   (garbage-collect))
 
