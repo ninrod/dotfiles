88ce59cc27d5e58ee5134399aac4f6d55b40155f
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-02-22 15:50:30 -0300

    add marvin scripts

---
+++ options/arch/scripts/s8/mount-galaxy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# $ adb devices
+# List of devices attached
+# ce03171370c7970705	device
+
+# temp marvin@zen $ adb shell dumpsys power| grep lock
+#   mHoldingWakeLockSuspendBlocker=true
+#   mHoldingDisplaySuspendBlocker=true
+# Suspend Blockers: size=5
+
+
+function verify_galaxys8_connected {
+  adb devices | grep ce03171370c7970705 > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Galaxy S8 Connected!"
+  else
+    echo "Galaxy S8 DISCONNECTED! Aborting."
+    exit 1
+  fi
+}
+
+function verify_galaxys8_locked {
+  # string='mHoldingWakeLockSuspendBlocker=true'
+  string='SCREEN_BRIGHT_WAKE_LOCK'
+  adb shell dumpsys power | grep $string
+
+  if [ $? -eq 0 ]; then
+    echo "Device is Locked. Unlock it first!"
+    exit 1
+  else
+    echo "Device is unlocked!"
+  fi
+}
+
+#verify_galaxys8_connected
+verify_galaxys8_locked
+
+
+MOUNT_POINT=~/s8
+mkdir -p $MOUNT_POINT
+go-mtpfs $MOUNT_POINT
+
+if [ $? -eq 0 ]; then
+  echo "Command succeeded."
+else
+  echo "Command Failed. Verify Phone Permission Dialog and try again."
+  exit 1
+fi
+
+
---
+++ options/arch/scripts/s8/umount-galaxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+fusermount -u ~/s8
+rm -r ~/s8
