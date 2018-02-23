c147128242a1df9d1e3f47582ea36ce2e65e31ef
ninrod <filipe.silva@gmail.com>
2018-02-23 00:20:43 -0300

    update s8 mount script

--- options/arch/scripts/s8/mount-galaxy.sh
+++ options/arch/scripts/s8/mount-galaxy.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # $ adb devices
 # List of devices attached
 # ce0917199c15b0700d      device
-
-# temp marvin@zen $ adb shell dumpsys power| grep lock
-#   mHoldingWakeLockSuspendBlocker=true
-#   mHoldingDisplaySuspendBlocker=true
-# Suspend Blockers: size=5
-
-
-function verify_galaxys8_connected {
+verify_galaxys8_connected() {
   adb devices | grep ce0917199c15b0700d > /dev/null
   if [ $? -eq 0 ]; then
     echo "Galaxy S8 Connected!"
@@ -20,8 +13,7 @@ function verify_galaxys8_connected {
   fi
 }
 
-function verify_galaxys8_locked {
-  # string='mHoldingWakeLockSuspendBlocker=true'
+verify_galaxys8_locked() {
   string='SCREEN_BRIGHT_WAKE_LOCK'
   adb shell dumpsys power | grep $string
 
@@ -33,12 +25,12 @@ function verify_galaxys8_locked {
   fi
 }
 
-#verify_galaxys8_connected
+verify_galaxys8_connected
 verify_galaxys8_locked
 
 MOUNT_POINT=~/s8
 mkdir -p $MOUNT_POINT
-go-mtpfs $MOUNT_POINT &
+aft-mtp-mount $MOUNT_POINT
 
 if [ $? -eq 0 ]; then
   echo "Command succeeded."
@@ -46,5 +38,3 @@ else
   echo "Command Failed. Verify Phone Permission Dialog and try again."
   exit 1
 fi
-
-
