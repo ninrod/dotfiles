3a3f3ba3d7d3162bd5684700bff56bc05d93aa0b
ninrod <filipe.silva@gmail.com>
2018-02-26 14:40:19 -0300

    add network manager configurations

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -24,4 +24,3 @@ alias mpdcli='ncmpcpp -b ~/.config/ncmpcpp/bindings.conf'
 alias nj='sudo netctl stop-all; sudo netctl start juliforever'
 alias ns='sudo netctl stop-all; sudo netctl start s8'
 alias nb='sudo netctl stop-all; sudo netctl start bcbWifi'
-
