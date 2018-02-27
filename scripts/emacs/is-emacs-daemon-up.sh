1a17e7b41262afcd2fda34035e1df7ab9fc50b61
ninrod <filipe.silva@gmail.com>
2018-02-27 17:43:47 -0300

    more polybar tweaks

--- scripts/emacs/is-emacs-daemon-up.sh
+++ scripts/emacs/is-emacs-daemon-up.sh
@@ -3,4 +3,6 @@
 # taken from: http://fontawesome.io/cheatsheet/
 # other: 
 
-emacsclient -e '(daemonp)' > /dev/null 2>&1 && echo %{F#39a} || echo %{F#d00}
+# green = #00cd66
+# red = #fb4934
+emacsclient -e '(daemonp)' > /dev/null 2>&1 && echo %{F#00cd66} || echo %{F#fb4934}
