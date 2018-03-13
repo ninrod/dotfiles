37b0cbfeab4672dcdfd4131dbea88e6aa0f4f1e0
ninrod <filipe.silva@gmail.com>
2018-03-13 10:00:26 -0300

    polybar: tweak colors

--- scripts/emacs/is-emacs-daemon-up.sh
+++ scripts/emacs/is-emacs-daemon-up.sh
@@ -6,4 +6,4 @@
 # green = #00cd66
 # red = #fb4934
 # emacs = #873eb4
-emacsclient -e '(daemonp)' > /dev/null 2>&1 && echo %{F#b16286} || echo %{F#fb4934}
+emacsclient -e '(daemonp)' > /dev/null 2>&1 && echo %{F#427b58} || echo %{F#fb4934}
