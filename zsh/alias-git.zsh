0abf58b27e76a6dfee381c6842f8d4d96f28d345
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-03-21 18:13:05 -0300

    fix gs and gg alias

--- zsh/alias-git.zsh
+++ zsh/alias-git.zsh
@@ -1,6 +1,5 @@
 # check
 alias g='git status -sb'
-alias gs='git status'
 alias gv='git branch -vv'
 alias gi='builtin cd $(git rev-parse --show-toplevel)'
 
@@ -21,9 +20,8 @@ ealias gr='git reset'
 # branch
 ealias gb='git branch'
 ealias gco='git checkout'
-# alias gg='git branch -vv'
-# with tip from https://stackoverflow.com/a/10708326/4921402
-alias gg='nohup gitg > /dev/null 2>&1 &'
+alias gg='git branch -vv'
+alias gs='nohup gitg > /dev/null 2>&1 &'
 
 # log
 alias git_log_custom='git log --pretty=format:"%C(yellow)%h%Creset %C(cyan)%>(14)%ad%Creset %C(magenta)%<(7)%an%Creset %C(blue)%d%Creset %C(white)%s%Creset" --abbrev=7 '
@@ -34,9 +32,7 @@ alias glr='gld --date=relative --all --reflog'
 alias glnc='git log --no-color'
 alias glt="git log --no-color --date-order --tags --simplify-by-decoration --pretty=format:'%ai %h %d' --date=format:'%Y-%m-%d %H:%M:%S' | grep --color tag"
 
-
 # ls-files
 alias glsu='git ls-files --others --exclude-standard'
 alias glsi='git ls-files -oiX .gitignore'
-
 
