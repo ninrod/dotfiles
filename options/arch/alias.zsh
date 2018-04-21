fdb07a36fa88a9b2d58cf707169edc72338084d4
ninrod <filipe.silva@gmail.com>
2018-04-20 21:55:33 -0300

    update mupdf

--- options/arch/alias.zsh
+++ options/arch/alias.zsh
@@ -17,7 +17,7 @@ alias sd='source deactivate'
 # random
 alias xclip="xclip -selection c"
 alias bat="sudo tlp stat -b"
-alias pdf="mupdf"
+alias pdf="evince"
 alias open=xdg-open
 alias mpdcli='ncmpcpp -b ~/.config/ncmpcpp/bindings.conf'
 
