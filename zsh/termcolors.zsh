81e0f8a3eda62959d3e0f9e302fccff0833eb134
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-05-29 20:18:21 -0300

    document #134 zsh color feature. kudos to @kutsan

--- zsh/termcolors.zsh
+++ zsh/termcolors.zsh
@@ -1,4 +1,14 @@
 # terminal colors
+
+# ATTENTION: from https://github.com/ninrod/dotfiles/issues/134 we know that
+# this could be replace by:
+# autload -U colors
+# colors
+# then we could use colors like this:
+# echo "${fg[yellow]}  Example text.  ${reset_color}"
+# echo "${fg_bold[yellow]}  Example text.  ${reset_color}"
+# echo "${bg_bold[yellow]}  Example text.  ${reset_color} ${bg[yellow]}${bg_bold[black]} Another text. ${reset_color}"
+
 TC='\e['
 Rst="${TC}0m"     # Reset all coloring and style
 Black="${TC}30m";
