1745be4f9ba4a79d9c14d02d7f5bb767f51d2f4e
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2018-05-30 18:24:41 -0300

    remove gradle zsh completion because zsh-users already provide it

--- boot/zsh-deps.zsh
+++ boot/zsh-deps.zsh
@@ -10,7 +10,6 @@ ningrab ninrod/docker-zsh-completion
 ningrab ninrod/docker-compose-zsh-completion
 ningrab zsh-users/zsh-completions
 ningrab ninrod/pass-zsh-completion
-ningrab ninrod/gradle-zsh-completion
 
 # appearance
 ningrab zdharma/fast-syntax-highlighting
