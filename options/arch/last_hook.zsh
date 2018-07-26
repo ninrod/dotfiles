0847eb29e4410f7580ba27264fed54fee2103758
ninrod <filipe.silva@gmail.com>
2018-07-26 10:14:49 -0300

    fix exercism zsh shell completion

--- options/arch/last_hook.zsh
+++ options/arch/last_hook.zsh
@@ -1,5 +1,5 @@
 # exercism
-exercism_completion_file=/usr/share/exercism-cli/completion/exercism_completion.zsh
+exercism_completion_file=~/.config/exercism/exercism_completion.zsh
 if [ -f $exercism_completion_file ]; then
   source $exercism_completion_file
 fi
