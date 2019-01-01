0c4f2c0fb2e2068ce4d38e2598481143f71e90ae
ninrod <filipe.silva@gmail.com>
2019-01-01 19:24:33 -0200

    include last hook support for ubuntu

--- options/user_last_hook.zsh
+++ options/user_last_hook.zsh
@@ -1,4 +1,6 @@
 unamestr=$(uname -a)
-if [[ $unamestr == *"Linux"* ]] && [[ $unamestr == *"ARCH"* ]]; then
+if [[ $unamestr:l == *"linux"* ]] && [[ $unamestr:l == *"arch"* ]]; then
   source ~/.options/arch/last_hook.zsh
+elif [[ $unamestr:l == *"ubuntu"* ]]; then
+  source ~/.options/ubuntu/last_hook.zsh
 fi
