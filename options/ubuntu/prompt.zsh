42db7737b73d6c976e2925982019de6fce1f8629
ninrod <filipe.silva@gmail.com>
2019-01-15 22:06:04 -0200

    tweak ubuntu prompt at droplet

--- options/ubuntu/prompt.zsh
+++ options/ubuntu/prompt.zsh
@@ -1,3 +1,9 @@
 # prompt config: nice unicode prompt chars: λ , › , ➤ , ∴ , → , ★ , ➜ , ❯, $
 DOT_PROMPT_CHAR="➜"
+
 SHELL_PROMPT_INFO_LEVEL="0"
+
+hn=$(hostname)
+if [[ $hn:l == *"droplet"* ]]; then
+  SHELL_PROMPT_INFO_LEVEL="1"
+fi
