2b6369af14ddb5f70b5a7775895cf81eb05a1df3
ninrod <filipe.silva@gmail.com>
2019-11-28 22:04:42 -0300

    init.el: document thin approach

--- emacs/init.el
+++ emacs/init.el
@@ -2,16 +2,23 @@
   (require 'package)
   (setq package-enable-at-startup nil)
 
-  ;;option 1: my fat local melpa/elpa mirror
+  ;; option 1: my fat local melpa/elpa mirror
   (defun ninrod--rsyncfy (repo) (concat "~/.dotfiles/deps/emacs/ninrod/rsynced-melpa/" repo))
   (setq package-archives `(("melpa" . ,(ninrod--rsyncfy "melpa/"))
                            ("org"   . ,(ninrod--rsyncfy "org/"))
                            ("gnu"   . ,(ninrod--rsyncfy "gnu/"))))
 
-  ;; option 2: my thin github mirror
-  ;;(setq package-archives
-  ;;      `(("myelpa" . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")))
+  ;; option 3: my thin local mirror
+  ;; (setq package-archives
+  ;;       `(("melpa" . "~/.emacs.d/thin-elpa-mirror")
+  ;;         ("org"   . "~/.emacs.d/thin-elpa-mirror")
+  ;;         ("gnu"   . "~/.emacs.d/thin-elpa-mirror")))
 
+  ;; option 2: my thin github mirror
+  ;; (setq package-archives
+  ;;       `(("melpa" . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")
+  ;;         ("org"   . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")
+  ;;         ("gnu"   . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")))
 
   (package-initialize)
   (unless (package-installed-p 'use-package)
