3ff46ec600780adae5dbeb88363d84cdc9fd35c8
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-12-06 12:39:56 -0300

    dynamically find my local elpa archives

--- emacs/init.el
+++ emacs/init.el
@@ -1,60 +1,56 @@
-(let ((gc-cons-threshold (* 25 1024 1024)))
+(let* ((gc-cons-threshold (* 25 1024 1024))
+       (local-elpa-mirror-base "~/.dotfiles/deps/emacs/ninrod")
+       (local-elpa-mirror-fat (concat  local-elpa-mirror-base "/rsynced-melpa"))
+       (local-elpa-mirror-thin (concat local-elpa-mirror-base "/thin-elpa-mirror"))
+       (local-elpa-not-installed (cond ((or (file-directory-p local-elpa-mirror-fat)
+                                            (file-directory-p local-elpa-mirror-thin))
+                                        nil)
+                                       (t t)))
+       (local-thin-installed (file-directory-p local-elpa-mirror-thin))
+       (local-fat-installed (file-directory-p local-elpa-mirror-fat)))
+
+  (when local-elpa-not-installed
+    (throw 'no-elpa "there are no elpa repos. aborting."))
+
   (require 'package)
   (setq package-enable-at-startup nil)
-
-  ;; option 1: my fat local melpa/elpa mirror
-  (defun ninrod--rsyncfy (repo) (concat "~/.dotfiles/deps/emacs/ninrod/rsynced-melpa/" repo))
-  (setq package-archives `(("melpa" . ,(ninrod--rsyncfy "melpa/"))
-                           ("org"   . ,(ninrod--rsyncfy "org/"))
-                           ("gnu"   . ,(ninrod--rsyncfy "gnu/"))))
-
-  ;; option 3: my thin local mirror
-  ;; (setq package-archives
-  ;;       `(("melpa" . "~/.emacs.d/thin-elpa-mirror")
-  ;;         ("org"   . "~/.emacs.d/thin-elpa-mirror")
-  ;;         ("gnu"   . "~/.emacs.d/thin-elpa-mirror")))
-
-  ;; option 2: my thin github mirror
-  ;; (setq package-archives
-  ;;       `(("melpa" . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")
-  ;;         ("org"   . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")
-  ;;         ("gnu"   . "https://raw.githubusercontent.com/ninrod/thin-elpa-mirror/master/")))
-
+  (cond (local-fat-installed
+         (setq package-archives `(("melpa" . ,(concat local-elpa-mirror-fat  "/melpa"))
+                                  ("org"   . ,(concat local-elpa-mirror-fat  "/org"))
+                                  ("gnu"   . ,(concat local-elpa-mirror-fat  "/gnu")))))
+        (local-thin-installed
+         (setq package-archives `(("melpa" . ,(concat local-elpa-mirror-thin))
+                                  ("org"   . ,(concat local-elpa-mirror-thin))
+                                  ("gnu"   . ,(concat local-elpa-mirror-thin))))))
   (package-initialize)
   (unless (package-installed-p 'use-package)
     (package-refresh-contents)
     (package-install 'use-package))
 
+  (setq vc-follow-symlinks "t") ; prevent prompt when loading org file
   (use-package diminish :ensure t)
-
   (use-package org
     :pin org
     :ensure org-plus-contrib
     :defer 7) ;; fetch latest version of `org-mode'
 
-  (setq vc-follow-symlinks "t") ; prevent prompt when loading org file
+  (defun ninrod/load-secrets-file (secrets-file)
+    (defun ninrod/parent-dir (dirname times)
+      "get the parent dir of DIRNAME"
+      (let* ((path (file-truename dirname)))
+        (dotimes (number times path)
+          (setq path (file-name-directory (directory-file-name path))))
+        path))
+    (let* ((nin-source-dir (ninrod/parent-dir user-emacs-directory 2))
+           (full-secrets-file (concat nin-source-dir secrets-file)))
+      (cond ((file-exists-p full-secrets-file)
+             (message "file `%s' EXISTS!!! loading..." full-secrets-file)
+             (org-babel-load-file full-secrets-file))
+            (t
+             (message "pre-secrets does NOT exist. nothing to do here.")))))
 
-  (defun ninrod/parent-dir (dirname times)
-    "get the parent dir of DIRNAME"
-    (let* ((path (file-truename dirname)))
-      (dotimes (number times path)
-        (setq path (file-name-directory (directory-file-name path))))
-      path))
-  (let ((secrets-file "secrets/emacs-pre.org"))
-    (cond ((file-exists-p (concat (ninrod/parent-dir user-emacs-directory 2) secrets-file))
-           (message "pre-secrets EXISTS!!! loading...")
-           (org-babel-load-file (expand-file-name secrets-file "~/code/sources")))
-          (t
-           (message "pre-secrets does NOT exist. nothing to see here."))))
-
+  (ninrod/load-secrets-file "secrets/emacs-pre.org")
   (org-babel-load-file (expand-file-name "boot.org" user-emacs-directory))
-
-  (let ((secrets-file "secrets/emacs-post.org"))
-    (cond ((file-exists-p (concat (ninrod/parent-dir user-emacs-directory 2) secrets-file))
-           (message "post-secrets EXISTS!!! loading...")
-           (org-babel-load-file (expand-file-name secrets-file "~/code/sources")))
-          (t
-           (message "post-secrets does NOT exist. nothing to see here."))))
+  (ninrod/load-secrets-file "secrets/emacs-pre.org")
 
   (garbage-collect))
-
