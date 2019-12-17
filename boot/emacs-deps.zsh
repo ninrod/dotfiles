fb7448decdbe09ac329ca5694b5788076428a529
ninrod <filipe.silva@gmail.com>
2019-12-17 13:45:08 -0300

    switch from fat melpa to thin-melpa for bootstrap

--- boot/emacs-deps.zsh
+++ boot/emacs-deps.zsh
@@ -2,7 +2,7 @@ mkdir -p $emacs_deps && cd $emacs_deps
 
 # my packages
 ningrab ninrod/gruvbox-theme
-ningrab ninrod/rsynced-melpa
+ningrab ninrod/thin-elpa-mirror
 ningrab ninrod/thin-elpa-mirror
 ningrab ninrod/evil-replace-with-char
 ningrab ninrod/evil-string-inflection
