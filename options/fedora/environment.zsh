8a0d146e1a45102a4c8dfed687e748081c420c22
ninrod <filipe.silva@gmail.com>
2021-09-19 12:26:09 -0400

    update fedora conf

--- options/fedora/environment.zsh
+++ options/fedora/environment.zsh
@@ -1,2 +1,2 @@
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-
+# gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+# gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt-alt', 'ctrl_modifier']"
