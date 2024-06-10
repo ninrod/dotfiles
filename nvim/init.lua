5432323b9cc395173d50fe5e905ca282ba144716
ninrod <filipe.silva@gmail.com>
2024-06-10 17:34:26 -0300

    funcionando, mas o mini.surround tá encrespando

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,26 +159,6 @@ vim.opt.scrolloff = 1
 -- [[ Basic Keymaps ]]
 require("keys.keybindings")
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
--- nvim/lua/keys/keybindings.lua
+++ nvim/lua/keys/keybindings.lua
@@ -8,7 +8,38 @@ vim.keymap.set("n", "<CR>", "<cmd>w<CR>")
 vim.keymap.set("n", "q", "ZQ")
 vim.keymap.set("n", "Q", "q")
 
-vim.keymap.set({ "o", "x" }, "(", "{")
-vim.keymap.set({ "o", "x" }, ")", "}")
+vim.keymap.set({ "n", "o", "x" }, "(", "{")
+vim.keymap.set({ "n", "o", "x" }, ")", "}")
 
 vim.keymap.set({ "n", "v" }, "gl", "G")
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "sh", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "sl", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "sj", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "sk", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "sx", "<C-w>s", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "sy", "<C-w>v", { desc = "Move focus to the upper window" })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
