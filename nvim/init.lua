f6f5d99a6c600b7799462d8fd986f5cb7a62f70d
ninrod <filipe.silva@gmail.com>
2024-06-10 17:36:45 -0300

    working

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,15 +159,6 @@ vim.opt.scrolloff = 1
 -- [[ Basic Keymaps ]]
 require("keys.keybindings")
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
