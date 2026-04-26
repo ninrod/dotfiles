2a4266b8da7c67b1d696cf459bce82fa5a87d873
ninrod <filipe.silva@gmail.com>
2026-04-26 20:33:24 -0300

    fix emacs config on fedora

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -10,61 +10,36 @@
    '((10 buffer-read-only font-lock-constant-face)
      (15
       (and buffer-file-name
-           (string-match ibuffer-compressed-file-name-regexp buffer-file-name))
+           (string-match ibuffer-compressed-file-name-regexp
+                         buffer-file-name))
       font-lock-doc-face)
-     (20
-      (string-match "^*"
-                    (buffer-name))
-      font-lock-comment-face)
+     (20 (string-match "^*" (buffer-name)) font-lock-comment-face)
      (25
-      (and
-       (string-match "^ "
-                     (buffer-name))
-       (null buffer-file-name))
+      (and (string-match "^ " (buffer-name)) (null buffer-file-name))
       italic)
-     (30
-      (memq major-mode ibuffer-help-buffer-modes)
-      font-lock-comment-face)
-     (35
-      (eq major-mode 'dired-mode)
-      font-lock-function-name-face)
+     (30 (memq major-mode ibuffer-help-buffer-modes)
+         font-lock-comment-face)
+     (35 (eq major-mode 'dired-mode) font-lock-function-name-face)
      (1
-      (or
-       (eq major-mode 'emacs-lisp-mode)
-       (eq major-mode 'lisp-mode))
+      (or (eq major-mode 'emacs-lisp-mode) (eq major-mode 'lisp-mode))
       highlight-numbers-number)
-     (1
-      (eq major-mode 'org-mode)
-      org-level-1)))
- '(package-selected-packages
-   '(rust-mode elpa-mirror rjsx-mode exec-path-from-shell nginx-mode ob-http racer dired-quick-sort systemd kotlin-mode circe pkgbuild-mode powershell flycheck-package pass docker general ivy-hydra which-key emmet-mode yasnippet smex groovy-mode feature-mode py-isort pyvenv yapfify company-anaconda anaconda-mode flycheck-rust cargo toml-mode go-guru go-eldoc go-mode yaml-mode dockerfile-mode gitattributes-mode gitignore-mode gitconfig-mode vimrc-mode json-mode json-reformat tide js2-mode web-mode markdown-mode f aggressive-indent helpful lispyville lispy worf neotree dired-sort counsel-projectile projectile vdiff-magit evil-magit magit git-gutter-fringe atomic-chrome disable-mouse ibuffer-vc editorconfig origami ag multi-term eyebrowse restart-emacs spaceline smartparens rainbow-mode rainbow-delimiters org-bullets lisp-extra-font-lock kurecolor info+ highlight-defined highlight-quoted highlight-parentheses highlight-numbers fic-mode autothemer all-the-icons evil-string-inflection evil-replace-with-char exato vdiff evil-ediff evil-lion evil-indent-plus evil-numbers evil-args evil-god-state evil-anzu evil-escape evil-visualstar evil-exchange evil-matchit evil-commentary evil-replace-with-register evil-surround evil-test-helpers org-plus-contrib diminish use-package))
+     (1 (eq major-mode 'org-mode) org-level-1)))
+ '(package-selected-packages nil)
  '(safe-local-variable-values
    '((eval when
-           (and
-            (buffer-file-name)
-            (file-regular-p
-             (buffer-file-name))
-            (string-match-p "^[^.]"
-                            (buffer-file-name)))
-           (unless
-               (featurep 'package-build)
-             (let
-                 ((load-path
-                   (cons "../package-build" load-path)))
+           (and (buffer-file-name) (file-regular-p (buffer-file-name))
+                (string-match-p "^[^.]" (buffer-file-name)))
+           (unless (featurep 'package-build)
+             (let ((load-path (cons "../package-build" load-path)))
                (require 'package-build)))
            (package-build-minor-mode)
-           (set
-            (make-local-variable 'package-build-working-dir)
-            (expand-file-name "../working/"))
-           (set
-            (make-local-variable 'package-build-archive-dir)
-            (expand-file-name "../packages/"))
-           (set
-            (make-local-variable 'package-build-recipes-dir)
-            default-directory))
-     (eval when
-           (fboundp 'rainbow-mode)
-           (rainbow-mode 1))
+           (set (make-local-variable 'package-build-working-dir)
+                (expand-file-name "../working/"))
+           (set (make-local-variable 'package-build-archive-dir)
+                (expand-file-name "../packages/"))
+           (set (make-local-variable 'package-build-recipes-dir)
+                default-directory))
+     (eval when (fboundp 'rainbow-mode) (rainbow-mode 1))
      (origami-fold-style . triple-braces))))
 
 (custom-set-faces
