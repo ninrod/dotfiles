379d428964ad1eab99b6c265499b20a3c6774caf
ninrod <filipe.silva@gmail.com>
2026-05-22 16:12:40 -0300

    emacs...

--- emacs/emacs-customizations.el
+++ emacs/emacs-customizations.el
@@ -10,36 +10,60 @@
    '((10 buffer-read-only font-lock-constant-face)
      (15
       (and buffer-file-name
-           (string-match ibuffer-compressed-file-name-regexp
-                         buffer-file-name))
+           (string-match ibuffer-compressed-file-name-regexp buffer-file-name))
       font-lock-doc-face)
-     (20 (string-match "^*" (buffer-name)) font-lock-comment-face)
+     (20
+      (string-match "^*"
+                    (buffer-name))
+      font-lock-comment-face)
      (25
-      (and (string-match "^ " (buffer-name)) (null buffer-file-name))
+      (and
+       (string-match "^ "
+                     (buffer-name))
+       (null buffer-file-name))
       italic)
-     (30 (memq major-mode ibuffer-help-buffer-modes)
-         font-lock-comment-face)
-     (35 (eq major-mode 'dired-mode) font-lock-function-name-face)
+     (30
+      (memq major-mode ibuffer-help-buffer-modes)
+      font-lock-comment-face)
+     (35
+      (eq major-mode 'dired-mode)
+      font-lock-function-name-face)
      (1
-      (or (eq major-mode 'emacs-lisp-mode) (eq major-mode 'lisp-mode))
+      (or
+       (eq major-mode 'emacs-lisp-mode)
+       (eq major-mode 'lisp-mode))
       highlight-numbers-number)
-     (1 (eq major-mode 'org-mode) org-level-1)))
+     (1
+      (eq major-mode 'org-mode)
+      org-level-1)))
  '(package-selected-packages nil)
  '(safe-local-variable-values
    '((eval when
-           (and (buffer-file-name) (file-regular-p (buffer-file-name))
-                (string-match-p "^[^.]" (buffer-file-name)))
-           (unless (featurep 'package-build)
-             (let ((load-path (cons "../package-build" load-path)))
+           (and
+            (buffer-file-name)
+            (file-regular-p
+             (buffer-file-name))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (unless
+               (featurep 'package-build)
+             (let
+                 ((load-path
+                   (cons "../package-build" load-path)))
                (require 'package-build)))
            (package-build-minor-mode)
-           (set (make-local-variable 'package-build-working-dir)
-                (expand-file-name "../working/"))
-           (set (make-local-variable 'package-build-archive-dir)
-                (expand-file-name "../packages/"))
-           (set (make-local-variable 'package-build-recipes-dir)
-                default-directory))
-     (eval when (fboundp 'rainbow-mode) (rainbow-mode 1))
+           (set
+            (make-local-variable 'package-build-working-dir)
+            (expand-file-name "../working/"))
+           (set
+            (make-local-variable 'package-build-archive-dir)
+            (expand-file-name "../packages/"))
+           (set
+            (make-local-variable 'package-build-recipes-dir)
+            default-directory))
+     (eval when
+           (fboundp 'rainbow-mode)
+           (rainbow-mode 1))
      (origami-fold-style . triple-braces))))
 
 (custom-set-faces
