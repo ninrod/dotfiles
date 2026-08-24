619fb696f923b6bd72af5b47d4bb5083efe4a586
ninrod <filipe.silva@gmail.com>
2026-08-23 21:45:04 -0300

    idea startup fix

--- options/wsl/scripts/idea/idea.sh
+++ options/wsl/scripts/idea/idea.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 
 # Inicia o IntelliJ desatrelado do terminal e silencia todos os logs de saída
-nohup /opt/idea-IU-262.9437.185/bin/idea "$@" >/dev/null 2>&1 &
+nohup /opt/idea/bin/idea "$@" >/dev/null 2>&1 &
 
 # Desassocia o processo filho da sessao atual do subshell
 disown
