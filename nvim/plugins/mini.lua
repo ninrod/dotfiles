97cc080796cd584d23899a9ac0949fc547fff8cf
ninrod <filipe.silva@gmail.com>
2026-09-07 18:23:36 -0300

    add ie and ae text objects through mini.ai

--- nvim/plugins/mini.lua
+++ nvim/plugins/mini.lua
@@ -1,7 +1,13 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		require("mini.ai").setup({ n_lines = 500 })
+		local mini_extra = require("mini.extra")
+		require("mini.ai").setup({
+			n_lines = 500,
+			custom_textobjects = {
+				e = mini_extra.gen_ai_spec.buffer(),
+			},
+		})
 
 		local statusline = require("mini.statusline")
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
