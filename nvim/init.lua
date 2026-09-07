ff59db86fbf480a92326581a04aaebe3b533b909
ninrod <filipe.silva@gmail.com>
2026-09-07 16:57:29 -0300

    add expandtabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,6 +38,10 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 1
