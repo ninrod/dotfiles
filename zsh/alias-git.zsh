6bd34a5f5e87ac10c3512db067886471671ca958
ninrod <filipe.silva@gmail.com>
2026-09-08 18:49:05 -0300

    add git config user email script alias

--- zsh/alias-git.zsh
+++ zsh/alias-git.zsh
@@ -50,4 +50,4 @@ alias git-ssh-fetch="~/code/secrets/scripts/git-ssh-fetch.sh"
 alias git-http-conf="~/code/secrets/scripts/git-http-config.sh"
 
 # git user email config: faz o repo usar as credenciais pessoais, e não as corporativas
-alias git-user-email-config="~/code/secrets/scripts/git-http-config.sh"
+alias git-user-email-config="~/code/secrets/scripts/git-user-email-config.sh"
