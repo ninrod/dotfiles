81aec15a12bc1ac5c95f2473a459218d0d5c85bc
ninrod <filipe.silva@gmail.com>
2026-09-10 11:57:28 -0300

    update deploy.zsh to deploy copilot-instructions.md on make

--- copilot/deploy.zsh
+++ copilot/deploy.zsh
@@ -1,2 +1,5 @@
 verifylink ~/.copilot/settings.json
 updatelinks ~/.copilot/settings.json copilot/settings.json
+
+verifylink ~/.copilot/copilot-instructions.md
+updatelinks ~/.copilot/copilot-instructions.md copilot/copilot-instructions.md
