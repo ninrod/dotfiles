7963663c93fe4a7b1a2076576481d02fe9fd11e4
ninrod <filipe.silva@gmail.com>
2026-09-17 22:34:35 -0300

    `e` is now a function. accepts piped instructions

--- zsh/alias-emacs.zsh
+++ zsh/alias-emacs.zsh
@@ -1,7 +1,32 @@
 # -*- mode: sh -*-
 
 # emacs client
-alias e="emacsclient --tty"
+e() {
+  if [[ -t 0 ]]; then
+    command emacsclient --tty "$@"
+    return
+  fi
+
+  if (( $# )); then
+    echo "e: piped input cannot be combined with arguments" >&2
+    return 2
+  fi
+
+  local input_file exit_status
+  input_file=$(mktemp "${TMPDIR:-/tmp}/emacs-stdin.XXXXXX") || return
+
+  cat > "$input_file"
+  exit_status=$?
+  if (( exit_status )); then
+    command rm -f -- "$input_file"
+    return "$exit_status"
+  fi
+
+  command emacsclient --tty "$input_file"
+  exit_status=$?
+  command rm -f -- "$input_file"
+  return "$exit_status"
+}
 alias et="emacsclient --tty"
 alias eg="background-connect-emacs-daemon.sh"
 
