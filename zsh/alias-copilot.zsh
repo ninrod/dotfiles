325eb16ee70e85c647e8bf594de9f99f9437f274
ninrod <filipe.silva@gmail.com>
2026-09-21 18:45:13 -0300

    fix yolo alias bug

--- zsh/alias-copilot.zsh
+++ zsh/alias-copilot.zsh
@@ -35,7 +35,7 @@ function yolo() {
     elif [ ! -t 0 ]; then
         command copilot "${copilot_options[@]}" --interactive "$(cat)"
     else
-        command copilot "${copilot_options[@]}" --interactive
+        command copilot "${copilot_options[@]}"
     fi
 }
 
